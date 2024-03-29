6725fb9dcd1c7ee472b2d67a74541933cfa0f1ba
git-devyani <164998967+git-devyani@users.noreply.github.com>
2024-03-29 07:49:05 +0000

    Dta Types And Variables Done

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+// console.log("Devyani Vashishth");
+const acc_id = '123432';
+let acc_name = 'Devyani Vashishth';
+var email_id = 'xyz@google.com';
+city = 'Jaipur'
+// acc_id = 2 / const variable change not allowed after decelaration
+console.log(acc_id);
+acc_name = 'DV';
+email_id = 'xyz@xyz.com';
+city = 'BLR';
+let acc_state;
+console.table([acc_id,acc_name,email_id,city,acc_state])
